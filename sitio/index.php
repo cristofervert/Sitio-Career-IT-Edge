8063efa93caa43644c203ff3cc8885fd806e2975
cristofer-vert <105964721+cristofer-vert@users.noreply.github.com>
2022-06-10 15:34:02 -0700

    Add files via upload

---
+++ sitio/index.php
@@ -0,0 +1,422 @@
+!DOCTYPE html
+html
+
+head
+  !-- Basic --
+  meta charset=utf-8 
+  meta http-equiv=X-UA-Compatible content=IE=edge 
+  !-- Mobile Metas --
+  meta name=viewport content=width=device-width, initial-scale=1, shrink-to-fit=no 
+  !-- Site Metas --
+  meta name=keywords content= 
+  meta name=description content= 
+  meta name=author content= 
+
+  titleCareer IT Edgetitle
+
+  !-- slider stylesheet --
+  link rel=stylesheet type=textcss href=httpscdnjs.cloudflare.comajaxlibsOwlCarousel22.1.3assetsowl.carousel.min.css 
+
+  !-- bootstrap core css --
+  link rel=stylesheet type=textcss href=cssbootstrap.css 
+
+  !-- fonts style --
+  link href=httpsfonts.googleapis.comcssfamily=Open+Sans400,700Poppins400,700Raleway400,700&display=swap rel=stylesheet 
+
+  !-- Custom styles for this template --
+  link href=cssstyle.css rel=stylesheet 
+  !-- responsive style --
+  link href=cssresponsive.css rel=stylesheet 
+head
+
+body
+  div class=hero_area
+    !-- header section strats --
+    header class=header_section
+      div class=container-fluid
+        nav class=navbar navbar-expand-lg custom_nav-container
+          a class=navbar-brand href=index.html
+            span
+            img src=.imageslogo.png alt=Career IT logo
+              Career IT Edge
+            span
+          a
+
+          div class=navbar-collapse id=
+            div class=custom_menu-btn
+              button onclick=openNav()
+                span class=s-1 span
+                span class=s-2 span
+                span class=s-3 span
+              button
+            div
+            div id=myNav class=overlay
+                div class=overlay-content
+                    a href=index.htmlInicioa
+                    a href=about.htmlSobre nosotrosa
+                    a href=feature.htmlServicios que ofrecemosa
+                    a href=login.htmlIniciar sesióna
+                div
+            div
+          div
+        nav
+      div
+    header
+    !-- end header section --
+
+    !-- slider section --
+    section class= slider_section position-relative
+      div class=container
+        div class=row
+          div class=col-md-7
+            div class=detail-box
+                h2
+                    ¿Eres Profesional en IT 
+                h2
+                h1
+                    ¡Te estamos buscando!
+                h1
+              div
+                a href=
+                  Leer más
+                a
+              div
+            div
+          div
+          div class=col-md-5
+            div id=carouselExampleIndicators class=carousel slide data-ride=carousel
+              ol class=carousel-indicators
+                li data-target=#carouselExampleIndicators data-slide-to=0 class=activeli
+                li data-target=#carouselExampleIndicators data-slide-to=1li
+                li data-target=#carouselExampleIndicators data-slide-to=2li
+              ol
+              div class=carousel-inner
+                div class=carousel-item active
+                  div class=img-box
+                    img src=imagessustituto.png alt= 
+                  div
+                div
+                div class=carousel-item
+                  div class=img-box
+                    img src=imagessustituto.png alt= 
+                  div
+                div
+                div class=carousel-item
+                  div class=img-box
+                    img src=imagessustituto.png alt= 
+                  div
+                div
+              div
+              a class=carousel-control-prev href=#carouselExampleIndicators role=button data-slide=prev
+                span class=sr-onlyPreviousspan
+              a
+              a class=carousel-control-next href=#carouselExampleIndicators role=button data-slide=next
+                span class=sr-onlyNextspan
+              a
+            div
+          div
+        div
+      div
+    section
+
+    !-- end slider section --
+  div
+
+      !-- about section --
+  section class=about_section layout_padding
+    div class=container
+      div class=heading_container d-flex justify-content-lg-start
+        h2 style=color #4282df 
+          Sobre nosotros
+        h2
+      div
+      div class=layout_padding2-top
+        div class=row
+            div class=col-md-5
+                div class=detail-box b-1
+                    p
+                        b
+                            Ofrecemos soluciones de RRHH
+                            en Tecnologías de la Información
+                            y Comunicación
+                        b                        
+                        br
+
+                        ul id=listaSobreNosotros
+                            li RPA, .NET, JAVA, PHYTON, ORACLE, WEB Developersli
+                            liBusiness Analystli
+                            liQuality Assuranceli
+                            liProject Managerli
+                            liSolution Architectli
+                            liScrum Masterli
+                            liOracle Developersli
+                            li Help Deskli
+                            liSAP CV'sli
+                            liNetwork and System Administratorli
+                            liBusiness Intelligenceli
+                            liSQL Developersli
+                        ul
+                    p
+                    p
+                        a href=
+                            Leer más
+                        a
+                    p
+                div
+            div
+          div class=col-md-5
+              
+              
+          div
+        div
+      div
+    div
+  section
+
+  !-- end about section --
+
+  !-- feature section --
+  section class=feature_section layout_padding2 layout_margin
+    div class=container
+      div class=heading_container
+        h2
+          Servicios que ofrecemos
+        h2
+      div
+    div
+    div class=container
+        div class=row
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        h6 align=center
+                            Publicación de Vacantes.
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Te apoyamos con todo el proceso de reclutamiento,
+                            desde la publicación de la vacante hasta
+                            la selección del candidato.
+                        p
+                    div
+                div
+            div
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        div class=img-box
+
+                        div
+                        h6
+                            Base de Datos de Profesionales IT
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Contamos con una extensa base de datos de Profesionales en IT,
+                            donde seguro encontrarás al candidato que tu empresa necesita.
+                        p
+                    div
+                div
+            div
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        div class=img-box
+
+
+                        div
+                        h6
+                            Aplicaciones psicométricas.
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Evaluamos a tus candidatos y te entregamos un reporte detallado.
+                        p
+                    div
+                div
+            div
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        div class=img-box
+
+
+                        div
+                        h6
+                            Evaluaciones socioeconómicas.
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Valida la competitividad de tus vacantes.
+                        p
+                    div
+                div
+            div
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        div class=img-box
+
+
+                        div
+                        h6
+                            Diagnóstico de nivel de inglés.
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Buscar descripción.
+                        p
+                    div
+                div
+            div
+            div class=col-lg-5
+                div class=box
+                    div class=head-box
+                        div class=img-box
+
+
+                        div
+                        h6
+                            Entrevistas generales yo técnicas.
+                        h6
+                    div
+                    div class=detail-box
+                        p
+                            Buscar descripción.
+                        p
+                    div
+                div
+            div
+        div
+    div
+    div class=d-flex justify-content-center
+      a href=
+        Leer más
+      a
+    div
+  section
+
+  !-- end feature section --
+  
+  !-- info section --
+  section class=info_section layout_padding-top
+    div class=info_logo-box
+      h2
+        Career IT Edge
+      h2
+    div
+    div class=container layout_padding2
+      div class=row
+        div class=col-md-4
+            h5
+                bServicios de valor a empresas en ITb
+            h5
+          ul class=listasPieDePag
+              liBase de Datos de Curriculums Profesionales en IT.li
+
+              liReportes de Nivel de Inglés de los Candidatos.li 
+
+              liReportes de Evaluaciones Psicometricas a Candidatos.li 
+                  
+              liReportes de Evaluaciones Socioeconómicas a Candidatos.li
+
+              liEvaluaciones de Clima Laboral.li
+
+              liFormas para Evaluaciones de Desempeño 360 en base a competencias Laborales.li
+           ul
+div
+        div class=col-md-4
+            h5
+                bServicios para profesionistas en ITb
+            h5
+          ul class=listasPieDePag
+              li
+                  Elaboración de Curriculum Vitae
+              li
+
+              li
+                  Promoción de curriculum con nuestros Socios en IT
+              li
+
+              li
+                  Evaluación de Nivel de Inglés
+              li
+
+              li
+                  Capacitación en Inglés
+              li
+
+              li
+                  Evaluaciones Psicometricas
+              li
+
+              li
+                  Inscripción a nuestro Blog.
+              li
+
+              li
+                  Capacitación y Asesoramiento en la búsqueda de empleo.
+              li
+          ul
+        div
+        div class=col-md-4
+            h5
+                bEncuentranos en redes socialesb
+            h5
+           
+             br
+            div class=social-box
+                a href=
+                    img src=imagesfb.png alt=
+                a
+                a href=
+                    img src=imagestwitter.png alt=
+                a
+                a href=
+                    img src=imageslinkedin.png alt=
+                a
+                a href=
+                    img src=imagesinstagram.png alt=
+                a
+             div
+             br
+                p style=font-size16px
+                    Queremos escucharte  br br
+                    info@career-it-edge.com
+
+                p
+           
+
+        div
+    div
+    div
+  section
+
+  !-- end info section --
+  !-- footer section --
+  section class=container-fluid footer_section
+    p
+      &copy; 2022 All Rights Reserved. Design by Gabriel and Cristofer
+    p
+  section
+  !-- footer section --
+
+  script type=textjavascript src=jsjquery-3.4.1.min.jsscript
+  script type=textjavascript src=jsbootstrap.jsscript
+
+  script
+    function openNav() {
+      document.getElementById(myNav).classList.toggle(menu_width);
+      document
+        .querySelector(.custom_menu-btn)
+        .classList.toggle(menu_btn-style);
+    }
+  script
+body
+
+html
